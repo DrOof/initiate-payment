1317b8cbd85a90d8d1dac190efb9b3613528fb7b
Olivier Hermanus <olivier@theweirdscience.com>
2020-02-12 20:32:43 +0100

    Cleaning up code

--- src/main/java/com/revolut/payment/event/Event.java
+++ src/main/java/com/revolut/payment/event/Event.java
@@ -2,6 +2,10 @@ package com.revolut.payment.event;
 
 public interface Event {
 
+    /**
+     * Gets the target that dispatched the event.
+     * @return
+     */
     public Object getTarget();
 
 }
